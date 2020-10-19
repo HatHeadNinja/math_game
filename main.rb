2773330eae18f43c0b879d86d8099f77da685081
Doug Dyson <dougdyson@gmail.com>
2020-10-19 14:54:35 +0000

    add players

--- main.rb
+++ main.rb
@@ -1,2 +1,11 @@
 require './questions.rb'
 require './players.rb'
+
+# create players
+player1 = Player.new('Player 1')
+player2 = Player.new('Player 2')
+
+puts player1.name
+puts player1.lives
+puts player2.name
+puts player2.lives
