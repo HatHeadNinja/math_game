6191d135c1521449b27058031f75d2e42a49f4d0
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-19 07:50:23 -0700

    add Question class

--- questions.rb
+++ questions.rb
@@ -1,1 +1,12 @@
-puts 'questions.rb'
+class Question
+  attr_accessor :answer, :question
+
+  def initialize
+    # add plus 1 because adding 0 in a question is too easy!
+    @num1 = rand(10) + 1
+    @num2 = rand(10) + 1
+    @answer = @num1 + @num2
+    @question = "What is #{@num1} + #{@num2} equal to?"
+  end
+
+end
